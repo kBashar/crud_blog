11ffac97f2a1d882b061c3bcbd861dcab3cafb53
Khyrul Bashar <kbashar@localhost.localdomain>
2018-10-12 17:42:57 +0600

    filter for offensive words, if offensive words are found user will be notified on those

--- blog/content/forms.py
+++ blog/content/forms.py
@@ -1,8 +1,19 @@
 from django.forms import ModelForm, Textarea
 from content.models import Blog
+from .offensive import check_offensive
 
 class BlogForm(ModelForm):
+    
+    def clean_title(self):
+            data = self.cleaned_data["title"]
+            return check_offensive(data)
 
+    def clean_content(self):
+        data = self.cleaned_data["content"]
+        return check_offensive(data)
+    
+
+        
     class Meta:
         model = Blog
         fields = ["title", "content"]
