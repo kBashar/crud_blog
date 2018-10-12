ae4a880561728c39a83223eedb3effe63e09aaf9
Khyrul Bashar <kbashar@localhost.localdomain>
2018-10-12 12:58:43 +0600

    Textarea widget for content field in blog creation

--- blog/content/forms.py
+++ blog/content/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from content.models import Blog
 
 class BlogForm(ModelForm):
@@ -6,4 +6,7 @@ class BlogForm(ModelForm):
     class Meta:
         model = Blog
         fields = ["title", "content"]
+        widgets = {
+                "content": Textarea(attrs={'cols': 80, 'rows': 20})
+                }
 
